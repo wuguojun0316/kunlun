4de3f6541c7fa4d6f6545d44a0df61aafadb4afc
wuguojun0316 <496697468@qq.com>
2017-04-27 15:19:22 +0800

    继承AppcompactActivity。

--- app/src/main/java/com/liangcang/kunlun/activity/ZxingActivity.java
+++ app/src/main/java/com/liangcang/kunlun/activity/ZxingActivity.java
@@ -1,11 +1,19 @@
 package com.liangcang.kunlun.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * 二维码扫描宿主Activity
  * Created by Wuguojun on 17/4/27.
  */
 
-public class ZxingActivity
+public class ZxingActivity extends AppCompatActivity
 {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
 }
